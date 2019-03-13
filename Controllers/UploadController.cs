d39fcb4080868cff024e1d3b1e8b27757b9dc9c1
Vic Chimenti <chimenti@seattleu.edu>
2019-03-13 11:49:24 -0700

    added upload controller

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlipPic.Controllers
+{
+    public class UploadController : Controller
+    {
+        public HttpResponseMessage Post()  
+        {  
+            HttpResponseMessage result = null;  
+            var httpRequest = HttpContext.Current.Request;  
+            if (httpRequest.Files.Count > 0)  
+            {  
+                var docfiles = new List<string>();  
+                foreach (string file in httpRequest.Files)  
+                {  
+                    var postedFile = httpRequest.Files[file];  
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);  
+                    postedFile.SaveAs(filePath);  
+                    docfiles.Add(filePath);  
+                }  
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);  
+            }  
+            else  
+            {  
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);  
+            }  
+            return result;  
+        }
+    }
+}
